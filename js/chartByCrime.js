2d5b03dcedfd1923334e1f264fe575a949b022e1
Elizabeth Benefiel <lizardfield@gmail.com>
2015-04-29 19:09:29 -0500

    cleaning up

--- js/chartByCrime.js
+++ js/chartByCrime.js
@@ -1,52 +1,70 @@
-var drawGraph = function(data) {
 
-	var currentData = []
-	var currentDay = this.start;
-	console.log(currentDay);
 
-	// iterate through each value
-	for (var i = 0; i < data.length; i++) {
-		currentData.push(data[i].number);
+var Graph1 = { 
+  /*graph_element: null,
+  graph_start: null,
+  graph_end: null,
+  graph_catID: null, */
+  INCREMENT_NUM_HOURS: 24,
 
-		// update the date so as to line up with it's value
-		currentDay.setHours(currentDay.getHours() + 24)
-		console.log(currentDay);
-	};
+  drawGraph: function(data) {
+    var GRAPH_WIDTH = 600;
+    var currentData = []
+    // var currentDay = this.start;
+    // console.log(currentDay);
 
-	var x = d3.scale.linear()
-	    .domain([0, d3.max(currentData)])
-	    .range([0, 420]);
+    // iterate through each value
+    for (var i = 0; i < data.length; i++) {
+      currentData.push(data[i].number);
 
-	var xAxis = d3.svg.axis()
+      // update the date so as to line up with it's value
+      // currentDay.setHours(currentDay.getHours() + this.increment);
+      // console.log(currentDay);
+    };
+
+    console.log(currentData);
+
+    var x = d3.scale.linear()
+        .domain([0, d3.max(currentData)])
+        .range([0, GRAPH_WIDTH]);
+
+    var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 
-	d3.select(".graph1")
-	  .selectAll("div")
-	    .data(currentData)
-	  .enter().append("div")
-	    .style("width", function(d) { return d + "px"; })
-	    .text(function(d) { return d; });
+    var selection = d3.select(".graph1")
+                      .selectAll("div")
+                      .data(currentData);
 
-};
+    console.log(selection);
 
-var Graph1 = { 
-  graph_element: null,
-  graph_start: null,
-  graph_end: null,
-  graph_catID: null,
-  
+    selection
+      .exit().remove();
+
+    selection
+      .enter().append("div")
+        .style("width", function(d) { return x(d) + "px"; })
+        .text(function(d) { return d; });
+    
+
+  },
+  /*
   display: function(start, end, catID) {
     this.graph_start = start;
     this.graph_end = end;
-    this.graph_catID = catID
+    this.graph_catID = catID;
     this.createGraph();
   },
+  */
   
-  createGraph: function() {
+  display: function(start, end, catID) {
 
-	reqMaker.crime_count_increment(24, this.graph_start, this.graph_end, null, this.graph_catID, null, drawGraph.bind({start: this.graph_start, end: this.graph_end, increment: 24})); 
-
+    reqMaker.crime_count_increment(24, start, 
+                                   end, null, catID, 
+                                   null, 
+                                   this.drawGraph.bind({start: start, 
+                                                        end: end, 
+                                                        increment: this.INCREMENT_NUM_HOURS})); 
   } 
 }
 
@@ -55,4 +73,3 @@ $().ready(function () {
     missionControl.addClient(theGraphObject.display.bind(theGraphObject));
 });
 
-$("#updateButton").click()
