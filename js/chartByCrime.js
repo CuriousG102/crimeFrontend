bab0d15e7048449ffdd1b90637c1b990eeeca6a9
Elizabeth Benefiel <lizardfield@gmail.com>
2015-04-30 01:25:58 -0500

    make graph have vertical bars

--- js/chartByCrime.js
+++ js/chartByCrime.js
@@ -1,60 +1,98 @@
-
-
-var Graph1 = { 
-  /*graph_element: null,
-  graph_start: null,
-  graph_end: null,
-  graph_catID: null, */
+var Graph1 = {
   INCREMENT_NUM_HOURS: 24,
 
   drawGraph: function(data) {
     var GRAPH_WIDTH = 600;
+    var GRAPH_HEIGHT = 500;
     var currentData = []
-    // var currentDay = this.start;
-    // console.log(currentDay);
+    var currentDay = this.start;
+
+    var days = [currentDay]
 
     // iterate through each value
     for (var i = 0; i < data.length; i++) {
       currentData.push(data[i].number);
 
       // update the date so as to line up with it's value
-      // currentDay.setHours(currentDay.getHours() + this.increment);
-      // console.log(currentDay);
+      currentDay.setHours(currentDay.getHours() + this.increment);
+      days.push(currentDay);
     };
 
-    var x = d3.scale.linear()
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+                  width = GRAPH_WIDTH - margin.left - margin.right,
+                  height = GRAPH_HEIGHT - margin.top - margin.bottom;
+
+    /* var x = d3.scale.linear()
         .domain([0, d3.max(currentData)])
-        .range([0, GRAPH_WIDTH]);
+        .range([0, width]); */
+
+    var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1)
+            .domain( function(days) {
+              for (var i = 0; i < days.length - 1; i++) {
+                return days[i];
+              };
+            });
+
+    var y = d3.scale.linear()
+            .range([height, 0])
+            .domain([0, d3.max(currentData)]);
+
+    console.log(height, width);
 
     var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
     var selection = d3.select(".graph1")
+                    .attr("width", GRAPH_WIDTH)
+                    .attr("height", GRAPH_HEIGHT)
+                  .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    selection.append("g")
+             .attr("class", "x axis")
+             .attr("transform", "translate(0," + height + ")")
+             .call(xAxis);
+
+    selection.append("g")
+             .attr("class", "y axis")
+             .call(yAxis);
+
+   /* selection = d3.select(".graph1")
                       .selectAll("div")
-                      .data(currentData);
+                      .data(currentData); */
+
+    var bar = selection.selectAll("g")
+              .data(currentData)
+            .enter().append("g")
+              .attr("transform", function(d) { return "translate(" + x(currentDay) + ",0)"; });
+
+    selection.selectAll(".bar")
+             .data(currentData)
+          .enter().append("rect")
+             .attr("class", "bar")
+             .attr("x", function(d) { return x(days); })
+             .attr("y", function(d) { return y(d); })
+             .attr("height", function (d) { return height - y(d); })
+             .attr("width", x.rangeBand());
 
     selection
-      .enter().append("div")
-        .style("width", function(d) { return x(d) + "px"; })
-        .text(function(d) { return d; });
-
-    selection.style("width", function(d) { return x(d) + "px"; })
-      .text(function(d) { return d; });
+             .attr("class", "bar")
+             .attr("x", function(d) { return x(days); })
+             .attr("y", function(d) { return y(d); })
+             .attr("height", function (d) { return height - y(d); })
+             .attr("width", x.rangeBand());
 
     selection
       .exit().remove();
     
 
   },
-  /*
-  display: function(start, end, catID) {
-    this.graph_start = start;
-    this.graph_end = end;
-    this.graph_catID = catID;
-    this.createGraph();
-  },
-  */
   
   display: function(start, end, catID) {
 
